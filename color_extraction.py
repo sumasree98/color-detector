6bf4c8a25980df083b6645155544707f34e0d14a
Suma Sree Thota <sumasree98@gmail.com>
2019-12-23 09:53:20 +0530

    added web cam capture

--- color_extraction.py
+++ color_extraction.py
@@ -1,15 +1,15 @@
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 from collections import Counter
 import cv2
 import numpy as np
 
-def get_dominant_color(image, k=4, image_processing_size = None):
+def get_dominant_color(image, k, image_processing_size = None):
     if image_processing_size is not None:
         image = cv2.resize(image, image_processing_size, 
                             interpolation = cv2.INTER_AREA)
     
     image = image.reshape((image.shape[0] * image.shape[1], 3))
-
     clt = KMeans(n_clusters = k)
     labels = clt.fit_predict(image)
 
@@ -20,8 +20,21 @@ def get_dominant_color(image, k=4, image_processing_size = None):
         dominant_colors.append(clt.cluster_centers_[element[0]])
     return dominant_colors
 
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+cv2.imshow("capture",frame)
+cv2.waitKey(0)
+#while(True):
+#    ret, frame = cap.read()
+#    cv2.imshow('frame',frame)
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#        break
+cap.release()
+cv2.destroyAllWindows()
+
 image_processing_size=(500,500)
-image = cv2.imread('image6.jpeg')
+image = frame
+#image = cv2.imread('image5.jpeg')
 image = cv2.resize(image, image_processing_size, interpolation = cv2.INTER_AREA)
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 dom_colors = get_dominant_color(hsv_image, k=5)
